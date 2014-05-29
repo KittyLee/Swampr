93d7c4fadf9335e8b5dcf88a0821c45d49d9b110
KittyLee <katherine@codeforprogress.org>
2014-05-29 16:38:44 -0400

    practice blog

---
+++ app/models.py
@@ -0,0 +1,6 @@
+class Post:
+	def __init__(self, title, author, content): #the initialization method
+		self.title = title   #Defining the title attribute
+		self.author = author #Defining the author attribute
+		self.content = content #Defining the content attribute
+
