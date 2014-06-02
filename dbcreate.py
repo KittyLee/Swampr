f07ee9b32acafc6b7f31fe5a45dd7a5ea9c93160
KittyLee <katherine@codeforprogress.org>
2014-06-02 15:59:28 -0400

    updated files

---
+++ dbcreate.py
@@ -0,0 +1,8 @@
+# Go ahead and make my day....tabase
+
+# Creates the database based on the models we've defined in models.py
+
+
+from config import SQLALCHEMY_DATABASE_URI
+from app import db 
+db.create_all()
